28f58554d2fccccd9e5d54aeedfe87d9b0334b0d
Eddie Cisneros <eddie.cisneros@agile-mi.com>
2025-10-16 13:16:37 -0400

    test

--- install.sh
+++ install.sh
@@ -28,4 +28,13 @@ sudo ufw status
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 
+#AWS CLI Install 
+sudo apt update
+sudo apt install -y unzip curl
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+
 
